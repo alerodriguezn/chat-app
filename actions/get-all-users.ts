6f98de967db5f377e0ca7e25fbe04ca660e783f2
Deivid <deividmg02@gmail.com>
2024-11-01 20:44:25 -0600

    feat: some actions were added

---
+++ actions/get-all-users.ts
@@ -0,0 +1,23 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export const getAllUsers = async () => {
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        emailVerified: true,
+        image: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return users;
+  } catch (error) {
+    console.error("Error al obtener los usuarios:", error);
+    throw new Error("No se pudieron recuperar los usuarios");
+  }
+};  
